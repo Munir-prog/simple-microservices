f2c9b1fd0d7af40015cbfc1b4083a3518c95e1e0
Munir-prog <munirprog@gmail.com>
2023-01-06 16:30:39 +0300

    model, controller and service for customer

---
+++ customer/src/main/java/com/mprog/customer/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.mprog.customer.service;
+
+import com.mprog.customer.http.request.CustomerRegistrationRequest;
+import com.mprog.customer.model.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+
+    public void register(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+    }
+}
